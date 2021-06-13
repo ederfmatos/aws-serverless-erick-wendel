d403378b009b9f476cc1172d97fef91c0892ff30
Eder Matos <ederfmatos@gmail.com>
2021-06-13 15:30:31 -0300

    iniciando com serverless framework

---
+++ demo-02-serverless-framework/run.sh
@@ -0,0 +1,11 @@
+#  instalar
+
+npm install -g serverless
+
+# invokar na Aws
+
+sls invoke -f <function-name>
+
+# invokar local
+
+sls invoke local -f <function-name> -l                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
